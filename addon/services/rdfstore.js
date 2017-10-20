3dffc938c3b857fd87574de811a59fece5850091
redpencil <info@redpencil.io>
2017-10-20 15:13:04 +0200

    Convert callbacks to promises in rdfstore service

--- addon/services/rdfstore.js
+++ addon/services/rdfstore.js
@@ -1,35 +1,40 @@
 import Ember from 'ember';
 import rdfstore from 'npm:rdfstore';
 
+const _proxyMethods = [
+  'clear',
+  'delete',
+  'execute',
+  'executeWithEnvironment',
+  'graph',
+  'insert',
+  'load',
+  'node',
+  'registeredGraphs'
+];
+
 export default Ember.Service.extend({
   init() {
     this._super(...arguments);
+
+    // dynamically add all proxy methods and wrap callback in Ember promise
+    _proxyMethods.forEach( (methodName) => {
+      this[methodName] = function() {
+        return new Ember.RSVP.Promise( (resolve, reject) => {
+          this.content[methodName](...arguments, (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      };
+    });
+
+    // create initial store
     rdfstore.create((err, store) => {
       this.content = store;
     });
-  },
-  clear() {
-    return this.content.clear(...arguments);
-  },
-  delete() {
-    return this.content.delete(...arguments);
-  },
-  execute() {
-    return this.content.execute(...arguments);
-  },
-  executeWithEnvironment() {
-    return this.content.executeWithEnvironment(...arguments);
-  },
-  graph() {
-    return this.content.graph(...arguments);
-  },
-  insert() {
-    return this.content.graph(...arguments);
-  },
-  load() {
-    return this.content.load(...arguments);
-  },
-  node() {
-    return this.content.node(...arguments);
   }
 });
